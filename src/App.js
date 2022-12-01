d003bb2c1cbee86fbbcb81103758c1a3f819af8c
BoognishSaves <johnpaulhaddad@hotmail.com>
2022-12-01 10:58:40 -0700

    Setting up Box component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import {Box} from "./components/Box.js"
 
 function App() {
   return (
     <div className="App">
-      Hello World 
+      <Box/>
     </div>
   );
 }
---
+++ src/components/Box.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import "../components/Box.css"
+
+export const Box = () => {
+    
+  return (
+    <button className="box">x</button>
+  )
+}
