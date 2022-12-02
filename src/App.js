be0bedb96578849f3f551179aa1279fae3cbdb76
BoognishSaves <johnpaulhaddad@hotmail.com>
2022-12-02 08:52:45 -0700

    All done score count fixed

--- src/App.js
+++ src/App.js
@@ -35,7 +35,7 @@ function App() {
     const winner = checkWinner(updatedBoard);
 
     if (winner) {
-      if(winner === "0") {
+      if(winner === "O") {
         let {oScore} = scores;
         oScore += 1
         setScores({...scores, oScore})
